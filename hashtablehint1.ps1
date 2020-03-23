c8fc23f92e6dac9a53f94e6ef6acdb0dbea1bf9f
AMadianov <40032430+AMadianov@users.noreply.github.com>
2020-03-23 22:33:29 +0300

    hash1

---
+++ hashtablehint1.ps1
@@ -0,0 +1,18 @@
+﻿$AllUsers = @{
+    'corp\mad1' = 'mad1@corp.local'
+    'corp\mad2' = 'mad2@corp.local'
+    'corp\mad3' = 'mad3@corp.local'
+    'corp\mad4' = 'mad4@corp.local'
+    'corp\mad5' = 'mad5@corp.local'
+}
+if ($AllUsers.Count -eq 0) {
+    write "True"
+} else {
+    $usrL = Read-Host 'What is loginname?'
+    $usrE = Read-Host 'What is E-mail?'
+    if ($AllUsers[$usrL]) {
+        write "this is user"
+    } else {
+        $AllUsers.Add($usrL, $usrE)
+    }
+}
